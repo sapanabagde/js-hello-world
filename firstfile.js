0826221fb223f619f214b9b46ff97920471b26dd
sapanabagde <sapana.bagde123@gmail.com>
2023-09-06 19:39:16 -0500

    changed sentence

--- firstfile.js
+++ firstfile.js
@@ -1,1 +1,11 @@
 console.log("Hello World, This is my new file");
+console.log("my first line")
+console.log("my second line");
+
+console.log("600+600=1200");
+ 
+/*
+Welcome, new coder. 
+Enter your code on line 7 to print 'Hello, World!' 
+to the console (the panel on the right)
+*/
