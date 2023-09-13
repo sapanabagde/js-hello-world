6e97ca1d0935b57c58745a1bd4e0b101bbfbfa33
sapanabagde <sapana.bagde123@gmail.com>
2023-09-13 15:51:28 -0500

    Exercise for Debugging Syntax Errors

--- Debugging-Syntax-Errors.js
+++ Debugging-Syntax-Errors.js
@@ -1,5 +1,73 @@
+/*let launchReady = false;
+let fuelLevel = 17000;
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}*/
+
 	
-let launchReady = false;
+/*let launchReady = false;
+let crewStatus = true;
+let computerStatus = 'green';
+
+if (crewStatus && computerStatus === 'green') {
+   console.log('Crew & computer cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   launchReady = false;
+}
+
+if (launchReady) {
+   console.log("10, 9, 8, 7, 6, 5, 4, 3, 2, 1...");
+   console.log("Fed parrot...");
+   console.log("Ignition...");
+   console.log("Liftoff!");
+} else {
+   console.log("Launch scrubbed.");
+}*/
+
+
+/*let launchReady = false;
+let fuelLevel = 17000;
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}*/
+
+
+	
+/*let launchReady = false;
+let fuelLevel = 27000;
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}
+if (launchReady) {
+   console.log("10, 9, 8...");
+   console.log("Fed parrot...");
+   console.log("6, 5, 4...");
+   console.log("Ignition...");
+   console.log("3, 2, 1...");
+   console.log("Liftoff!");
+} else {
+   console.log("Launch scrubbed.");
+}*/
+
+
+/*let launchReady = false;
 let fuelLevel = 17000;
 let crewStatus = true;
 let computerStatus = 'green';
@@ -19,6 +87,98 @@ if (crewStatus && computerStatus === 'green'){
    console.log('WARNING: Crew or computer not ready!');
    launchReady = false;
 }
+
+if (launchReady) {
+   console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+   console.log('Liftoff!');
+} else {
+   console.log('Launch scrubbed.');
+}*/
+
+
+/*let launchReady = false;
+let fuelLevel = 17000;
+// let crewStatus = true;
+// let computerStatus = 'green';
+
+if (fuelLevel >= 17000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}*/
+
+
+/*let launchReady = false;
+let fuelLevel = 17000;
+let crewStatus = true;
+let computerStatus = 'green';
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}
+
+if (crewStatus && computerStatus === 'green'){
+   console.log('Crew & computer cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   launchReady = false;
+}*/
+
+
+/*let launchReady = false;
+let fuelLevel = 17000;
+let crewStatus = true;
+let computerStatus = 'green';
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}
+console.log(launchReady);
+
+if (crewStatus && computerStatus === 'green'){
+   console.log('Crew & computer cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   launchReady = false;
+}
+console.log(launchReady);*/
+
+
+let launchReady = false;
+let crewReady = false;
+let fuelLevel = 17000;
+let crewStatus = true;
+let computerStatus = 'green';
+
+if (fuelLevel >= 17000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}
+console.log("launchReady = ", launchReady);
+
+if (crewStatus && computerStatus === 'green'){
+   console.log('Crew & computer cleared.');
+   crewReady = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   crewReady = false;
+}
+console.log("crewReady = ", crewReady);
 
 if (launchReady) {
    console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
