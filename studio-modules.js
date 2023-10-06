7a8218c856d9c5badd6bba956e208404f99aaa4b
sapanabagde <sapana.bagde123@gmail.com>
2023-10-05 19:47:53 -0500

    studio modules

---
+++ studio-modules.js
@@ -0,0 +1,70 @@
+// //1) Use code to print “Hello, World” to the screen.
+
+//  console.log("Hello, World!");
+
+//2) Define, initialize, change, and use variables.
+
+// let programmingLanguage = "JavaScript";
+// console.log(programmingLanguage);
+
+
+// 3) Convert the string '1234' into a number.
+   //console.log(String("1234"));
+ 
+
+//4)Construct a for loop to repeat a task 100 times.
+
+//  for (let i = 0; i <= 100; i++) {
+//     console.log(i);
+//  }
+
+        
+//5) Construct if/else if/else statements to decide which of     three tasks to perform.
+
+//  let x = 10;
+// let y = 20;
+
+// if (x > y) {
+//    console.log("x is greater than y");
+// } else if (x < y) {
+//    console.log("x is less than y");
+// } else {
+//    console.log("x and y are equal");
+// }
+
+// 6)Build, modify, and access an array.
+
+// let javaScriptFrameworks = ["React", "Angular", "Ember"];
+// console.log(javaScriptFrameworks);
+
+//Set the value of index 2 to be "Vue"
+// javaScriptFrameworks[2] = "Vue";
+
+// Notice the value at index 2 is now "Vue"
+// console.log(javaScriptFrameworks);
+
+
+//7)Design and call a function.
+
+// function sumToN(n) {
+//      let sum = 0;
+//     for (let i = 0; i <= n; i++) { //This is the example of sum to number CONCEPT and it will return 6
+
+//         sum += i;
+//     }
+//      return sum;
+//  }
+//  console.log(sumToN(3));
+
+
+// 8) Call one function from within another function.
+
+//      function printArray(names) {
+//     for (let i = 0; i < names.length; i++) {
+//         console.log(names[i]);
+//     }
+// }
+
+// printArray(["Lena", "James", "Julio"]);
+// console.log("---");
+// printArray(["orange", "apple", "pear"]);
