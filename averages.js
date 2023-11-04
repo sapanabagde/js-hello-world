e8c9ec4e2adf2cd85213536be97946e6671d655d
sapanabagde <sapana.bagde123@gmail.com>
2023-11-04 18:31:06 -0500

    Fetch-Exercise-Assignment

--- averages.js
+++ averages.js
@@ -1,25 +1,36 @@
 
 //  EXPORT FINISHED MODULES 1.  In averages.js, add code to export all of the functions within an object.
 
-function averageForStudent(nameIndex,scores){
-    let sum = 0;
-    for (let i=0; i<scores.length; i++){
-      sum += scores[nameIndex][i];
-    }
-    let average = sum/scores[nameIndex].length;
-    return average;
-  }
+// function averageForStudent(nameIndex,scores){
+//     let sum = 0;
+//     for (let i=0; i<scores.length; i++){
+//       sum += scores[nameIndex][i];
+//     }
+//     let average = sum/scores[nameIndex].length;
+//     return average;
+//   }
   
-  function averageForTest(testIndex,scores){
-    let sum = 0;
-    for (let i=0; i<scores.length; i++){
-      sum += scores[i][testIndex];
-    }
-    let average = sum/scores[0].length;
-    return average;
-  }
+//   function averageForTest(testIndex,scores){
+//     let sum = 0;
+//     for (let i=0; i<scores.length; i++){
+//       sum += scores[i][testIndex];
+//     }
+//     let average = sum/scores[0].length;
+//     return average;
+//   }
   
-  module.exports = {
-    averageForStudent: averageForStudent,
-    averageForTest: averageForTest
- };
+//   module.exports = {
+//     averageForStudent: averageForStudent,
+//     averageForTest: averageForTest
+//  };
+
+const person = {
+  name: ["Bob", "Smith"],
+  age: 32,
+  bio: function () {
+    console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+  },
+  introduceSelf: function () {
+    console.log(`Hi! I'm ${this.name[0]}.`);
+  },
+};
